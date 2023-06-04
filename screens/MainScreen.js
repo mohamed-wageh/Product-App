403783da2ed9addb11cfed0ab1e14d8500a1330b
mohamed-wageh <80712072+mohamed-wageh@users.noreply.github.com>
2023-06-04 13:43:36 +0200

    task done

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,10 +1,114 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import axios from "axios";
 
-export default function MainScreen() {
+const ProductScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/products");
+      setProducts(response.data.products);;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleProductPress = (product) => {
+    setProducts((prevProducts) =>
+      prevProducts.filter((p) => p.id !== product.id)
+    );
+  };
+
+  const sortProducts = () => {
+    const sortedProducts = [...products].sort((a, b) =>
+      b.title.localeCompare(a.title)
+    );
+    setProducts(sortedProducts);
+  };
+
+
+
+  const renderProductItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.productItem}
+      onPress={() => handleProductPress(item)}
+    >
+      <Image style={styles.thumbnail} source={{ uri: item.thumbnail }} />
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.price}>Price: ${item.price}</Text>
+        <Text style={styles.stock}>Stock: {item.stock}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <View>
-      <Text>This is the Main Screen</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.sortButton} onPress={sortProducts}>
+        <Text style={styles.sortButtonText}>Sort Alphabetically</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderProductItem}
+      />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  sortButton: {
+    marginBottom: 16,
+  },
+  sortButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "blue",
+  },
+  productItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  thumbnail: {
+    width: 80,
+    height: 80,
+    marginRight: 10,
+  },
+  detailsContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  description: {
+    marginBottom: 5,
+  },
+  price: {
+    fontWeight: "bold",
+  },
+  stock: {
+    fontStyle: "italic",
+  },
+});
+
+export default ProductScreen;
